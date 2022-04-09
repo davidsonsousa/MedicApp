8e3fbf2314f69f27ea7ef29130189ceedc3faaf7
Davidson Sousa <dev@davidsonsousa.net>
2022-04-09 22:06:24 +0200

    Added usings

--- src/ClinicManagement.Infrastructure/GlobalUsings.cs
+++ src/ClinicManagement.Infrastructure/GlobalUsings.cs
@@ -6,3 +6,4 @@ global using ClinicManagement.Infrastructure.Data.Configuration;
 global using MedicApp.SharedKernel;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Logging;
