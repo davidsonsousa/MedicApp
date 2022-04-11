8808b480909439d4705efda4fc99f9938f2c3b2d
Davidson Sousa <dev@davidsonsousa.net>
2022-04-11 14:32:02 +0200

    Added ClinicViewModel, ViewModelBase and refactored ClinicExtensions

--- src/ClinicManagement.ApplicationCore/Extensions/Mapper/ClinicExtensions.cs
+++ src/ClinicManagement.ApplicationCore/Extensions/Mapper/ClinicExtensions.cs
@@ -19,6 +19,24 @@ public static class ClinicExtensions
     }
 
     /// <summary>
+    /// Maps a Clinic entity to a ClinicViewModel object
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static ClinicViewModel MapToViewModel(this Clinic? item)
+    {
+        Guard.Against.Null(item, nameof(item));
+
+        return new ClinicViewModel
+        {
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Name = item.Name,
+            SelectedBranches = item.Branches.Select(b => b.VanityId).ToArray()
+        };
+    }
+
+    /// <summary>
     /// Maps a ClinicEditModel to a Clinic entity
     /// </summary>
     /// <param name="item"></param>
@@ -40,7 +58,7 @@ public static class ClinicExtensions
     /// <returns></returns>
     public static Clinic MapToEntity(this ClinicEditModel item, Clinic? clinic)
     {
-        Guard.Against.Null(clinic);
+        Guard.Against.Null(clinic, nameof(clinic));
 
         clinic.Id = item.Id;
         clinic.VanityId = item.VanityId;
---
+++ src/ClinicManagement.ApplicationCore/Models/ViewModels/ClinicViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ClinicManagement.ApplicationCore.Models.ViewModels;
+
+public class ClinicViewModel : ViewModelBase
+{
+    public string Name { get; set; } = string.Empty;
+
+    public IEnumerable<Guid> SelectedBranches { get; set; } = new List<Guid>();
+}
