a2c8b33a3258cfc98d2ce1817d3af8513ff425d7
Davidson Sousa <dev@davidsonsousa.net>
2022-04-11 09:56:43 +0200

    Added ClinicEditModel and extensions

---
+++ src/ClinicManagement.ApplicationCore/Extensions/Mapper/ClinicExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace ClinicManagement.ApplicationCore.Extensions.Mapper;
+
+public static class ClinicExtensions
+{
+    /// <summary>
+    /// Maps a Clinic entity to a ClinicEditModel object
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static ClinicEditModel MapToEditModel(this Clinic item)
+    {
+        return new ClinicEditModel
+        {
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Name = item.Name,
+            SelectedBranches = item.Branches.Select(b => b.VanityId).ToArray()
+        };
+    }
+
+    /// <summary>
+    /// Maps a ClinicEditModel to a Clinic entity
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static Clinic MapToEntity(this ClinicEditModel item)
+    {
+        return new Clinic
+        {
+            Id = item.Id,
+            VanityId = item.VanityId,
+            Name = item.Name
+        };
+    }
+
+    /// <summary>
+    /// Maps a ClinicEditModel to an existing Clinic entity
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static Clinic MapToEntity(this ClinicEditModel item, Clinic? clinic)
+    {
+        Guard.Against.Null(clinic);
+
+        clinic.Id = item.Id;
+        clinic.VanityId = item.VanityId;
+        clinic.Name = item.Name;
+
+        return clinic;
+    }
+}
---
+++ src/ClinicManagement.ApplicationCore/Models/EditModels/ClinicEditModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ClinicManagement.ApplicationCore.Models.EditModels;
+
+public class ClinicEditModel : EditModelBase
+{
+    public string Name { get; set; } = string.Empty;
+
+    public IEnumerable<Guid> SelectedBranches { get; set; } = new List<Guid>();
+}
