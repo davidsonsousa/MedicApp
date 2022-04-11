0a710ff862d0223b2541c696a220dcbb00d0e5f0
Davidson Sousa <dev@davidsonsousa.net>
2022-04-11 14:33:02 +0200

    Added global usings

--- src/ClinicManagement.ApplicationCore/GlobalUsings.cs
+++ src/ClinicManagement.ApplicationCore/GlobalUsings.cs
@@ -3,4 +3,6 @@ global using System.Linq.Expressions;
 global using Ardalis.GuardClauses;
 global using ClinicManagement.ApplicationCore.Entities;
 global using ClinicManagement.ApplicationCore.Models.EditModels;
+global using ClinicManagement.ApplicationCore.Models.ViewModels;
 global using MedicApp.SharedKernel;
+global using MedicApp.SharedKernel.Interfaces;
--- src/MedicApp.SharedKernel/GlobalUsings.cs
+++ src/MedicApp.SharedKernel/GlobalUsings.cs
@@ -2,4 +2,6 @@
 global using System.ComponentModel.DataAnnotations.Schema;
 global using Ardalis.GuardClauses;
 global using MedicApp.SharedKernel.Extensions;
+global using MedicApp.SharedKernel.Interfaces;
+global using Newtonsoft.Json;
 global using Newtonsoft.Json.Linq;
