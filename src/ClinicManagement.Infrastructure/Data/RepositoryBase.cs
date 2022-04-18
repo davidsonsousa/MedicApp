9c75f1fd2dd6a4cf58d20010869057e3b0ce5b98
Davidson Sousa <dev@davidsonsousa.net>
2022-04-19 00:02:21 +0200

    Refactored code

--- src/ClinicManagement.Infrastructure/Data/RepositoryBase.cs
+++ src/ClinicManagement.Infrastructure/Data/RepositoryBase.cs
@@ -5,7 +5,7 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
     protected readonly ClinicManagementContext DbContext;
     protected readonly ILogger Logger;
 
-    public RepositoryBase(ClinicManagementContext dbContext, LoggerFactory loggerFactory)
+    public RepositoryBase(ClinicManagementContext dbContext, ILoggerFactory loggerFactory)
     {
         Guard.Against.Null(dbContext, nameof(dbContext));
         DbContext = dbContext;
@@ -14,9 +14,9 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
 
     public async virtual Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        Guard.Against.Null(entity, nameof(entity));
+        Guard.Against.Null(entity, entity.GetType().Name);
 
-        Logger.LogDebug(Constants.DebugMessages.MethodObjectValue, nameof(AddAsync), nameof(entity), entity);
+        Logger.DebugMethodCall(nameof(AddAsync), entity);
 
         DbContext.Set<TEntity>().Add(entity);
         await SaveChangesAsync(cancellationToken);
@@ -25,7 +25,7 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
 
     public async virtual Task<int> CountAsync(CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.Method, nameof(CountAsync));
+        Logger.DebugMethodCall(nameof(CountAsync));
 
         return await DbContext.Set<TEntity>().CountAsync(cancellationToken);
     }
@@ -34,16 +34,16 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
     {
         Guard.Against.Null(filter, nameof(filter));
 
-        Logger.LogDebug(Constants.DebugMessages.MethodObjectValue, nameof(CountAsync), nameof(filter), filter);
+        Logger.DebugMethodCall(nameof(CountAsync), filter);
 
         return await DbContext.Set<TEntity>().CountAsync(filter, cancellationToken);
     }
 
     public async virtual Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        Guard.Against.Null(entity, nameof(entity));
+        Guard.Against.Null(entity, entity.GetType().Name);
 
-        Logger.LogDebug(Constants.DebugMessages.MethodObjectValue, nameof(DeleteAsync), nameof(entity), entity);
+        Logger.DebugMethodCall(nameof(DeleteAsync), entity);
 
         // We never delete anything, only update the IsDelete flag
         entity.IsDeleted = true;
@@ -52,28 +52,28 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
 
     public async virtual Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.Method, nameof(GetAllAsync));
+        Logger.DebugMethodCall(nameof(GetAllAsync));
 
         return await GetValidRecords().ToListAsync(cancellationToken);
     }
 
     public async virtual Task<TEntity?> GetByIdAsync(long id, CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.Method, nameof(GetByIdAsync));
+        Logger.DebugMethodCall(nameof(GetByIdAsync));
 
         return await DbContext.Set<TEntity>().FindAsync(new object[] { id }, cancellationToken);
     }
 
     public async virtual Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.Method, nameof(GetByIdAsync));
+        Logger.DebugMethodCall(nameof(GetByIdAsync));
 
         return await DbContext.Set<TEntity>().Where(q => q.VanityId == id).SingleOrDefaultAsync(cancellationToken);
     }
 
     public async virtual Task<IEnumerable<TEntity>> GetReadOnlyAsync(Expression<Func<TEntity, bool>> filter, CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.MethodObjectValue, nameof(GetReadOnlyAsync), nameof(filter), filter);
+        Logger.DebugMethodCall(nameof(GetReadOnlyAsync), filter);
 
         var records = GetValidRecords();
 
@@ -87,16 +87,16 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
 
     public async virtual Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.Method, nameof(SaveChangesAsync));
+        Logger.DebugMethodCall(nameof(SaveChangesAsync));
 
         return await DbContext.SaveChangesAsync(cancellationToken);
     }
 
     public async virtual Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        Guard.Against.Null(entity, nameof(entity));
+        Guard.Against.Null(entity, entity.GetType().Name);
 
-        Logger.LogDebug(Constants.DebugMessages.MethodObjectValue, nameof(UpdateAsync), nameof(entity), entity);
+        Logger.DebugMethodCall(nameof(UpdateAsync), entity);
 
         DbContext.Update(entity);
         await SaveChangesAsync(cancellationToken);
@@ -104,7 +104,7 @@ public abstract class RepositoryBase<TEntity> : IReadRepository<TEntity>, IChang
 
     private IQueryable<TEntity> GetValidRecords()
     {
-        Logger.LogDebug(Constants.DebugMessages.Method, nameof(GetValidRecords));
+        Logger.DebugMethodCall(nameof(GetValidRecords));
 
         return DbContext.Set<TEntity>().Where(q => q.IsDeleted == false);
     }
--- src/ClinicManagement.Infrastructure/Services/ClinicService.cs
+++ src/ClinicManagement.Infrastructure/Services/ClinicService.cs
@@ -2,62 +2,62 @@
 
 public class ClinicService : ServiceBase<Clinic>, IClinicService
 {
-    public ClinicService(IRepository<Clinic> repository, ILoggerFactory loggerFactory) : base(repository, loggerFactory)
+    public ClinicService(IClinicRepository clinicRepository, ILoggerFactory loggerFactory) : base(clinicRepository, loggerFactory)
     {
 
     }
 
-    public async Task<IReturnValue> GetClinicById(Guid id)
+    public async Task<IReturnValue> GetClinicById(Guid id, CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.ClassMethodObjectValue, nameof(ClinicService), nameof(GetClinicById), nameof(id), id);
+        Logger.DebugMethodCall(nameof(ClinicService), nameof(GetClinicById), id);
         var returnValue = new ReturnValue<ClinicViewModel>();
 
         try
         {
-            var clinic = await Repository.GetByIdAsync(id);
+            var clinic = await Repository.GetByIdAsync(id, cancellationToken);
             Guard.Against.Null(clinic, nameof(clinic));
 
             returnValue.Value = clinic.MapToViewModel();
         }
         catch (Exception ex)
         {
-            Logger.LogError(ex, Constants.DebugMessages.ClassMethod, nameof(ClinicService), nameof(GetClinicById));
+            Logger.ErrorMethodCall(ex, nameof(ClinicService), nameof(GetClinicById));
             returnValue.SetErrorMessage("An error has occurred while loading the clinic");
         }
 
         return returnValue;
     }
 
-    public async Task<IReturnValue> SaveAsync(ClinicEditModel model)
+    public async Task<IReturnValue> SaveAsync(ClinicEditModel model, CancellationToken cancellationToken = default)
     {
-        Logger.LogDebug(Constants.DebugMessages.ClassMethodObjectValue, nameof(ClinicService), nameof(SaveAsync), nameof(model), model);
+        Logger.DebugMethodCall(nameof(ClinicService), nameof(SaveAsync), model);
         var returnValue = new ReturnValue($"Clinic '{model.Name}' saved.");
 
         try
         {
-            await AddOrUpdateAsync(model);
-            await Repository.SaveChangesAsync();
+            await AddOrUpdateAsync(model, cancellationToken);
+            await Repository.SaveChangesAsync(cancellationToken);
         }
         catch (Exception ex)
         {
-            Logger.LogError(ex, Constants.DebugMessages.ClassMethod, nameof(ClinicService), nameof(SaveAsync));
+            Logger.ErrorMethodCall(ex, nameof(ClinicService), nameof(GetClinicById));
             returnValue.SetErrorMessage("An error has occurred while saving the clinic");
         }
 
         return returnValue;
     }
 
-    private async Task AddOrUpdateAsync(ClinicEditModel model)
+    private async Task AddOrUpdateAsync(ClinicEditModel model, CancellationToken cancellationToken = default)
     {
         if (model.IsNew)
         {
             var clinic = model.MapToEntity();
-            await Repository.AddAsync(clinic);
+            await Repository.AddAsync(clinic, cancellationToken);
         }
         else
         {
-            var clinic = model.MapToEntity(await Repository.GetByIdAsync(model.VanityId));
-            await Repository.UpdateAsync(clinic);
+            var clinic = model.MapToEntity(await Repository.GetByIdAsync(model.VanityId, cancellationToken));
+            await Repository.UpdateAsync(clinic, cancellationToken);
         }
     }
 }
