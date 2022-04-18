e5109defee8e6fbb681a1f6e18ddec862229c2df
Davidson Sousa <dev@davidsonsousa.net>
2022-04-19 00:00:28 +0200

    Refactored Address class

--- src/MedicApp.SharedKernel/Address.cs
+++ src/MedicApp.SharedKernel/Address.cs
@@ -1,15 +1,14 @@
-﻿namespace MedicApp.SharedKernel
+﻿namespace MedicApp.SharedKernel;
+
+public class Address : ValueObject<Address>
 {
-    public class Address : ValueObject<Address>
-    {
-        public string Street { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
 
-        public string City { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
 
-        public string? State { get; set; }
+    public string? State { get; set; }
 
-        public string Country { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
 
-        public string ZipCode { get; set; } = string.Empty;
-    }
+    public string ZipCode { get; set; } = string.Empty;
 }
