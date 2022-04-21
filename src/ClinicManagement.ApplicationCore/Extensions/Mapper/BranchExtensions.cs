9ba3cc39792a508ac4b5984b77affbd04fa67591
Davidson Sousa <dev@davidsonsousa.net>
2022-04-21 16:54:30 +0200

    Added BranchExtensions, BranchRequest and BranchResponse

---
+++ src/ClinicManagement.ApplicationCore/Extensions/Mapper/BranchExtensions.cs
@@ -0,0 +1,92 @@
+﻿namespace ClinicManagement.ApplicationCore.Extensions.Mapper;
+
+public static class BranchExtensions
+{
+    /// <summary>
+    /// Maps a Branch entity to a BranchRequest object
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static BranchRequest MapToRequest(this Branch item)
+    {
+        return new BranchRequest
+        {
+            VanityId = item.VanityId,
+            Name = item.Name,
+            Address = item.Address,
+            ClinicId = item.Clinic.VanityId,
+            SelectedDepartments = item.Departments.Select(b => b.VanityId).ToArray()
+        };
+    }
+
+    /// <summary>
+    /// Maps a Branch entity to a BranchResponse object
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static BranchResponse MapToResponse(this Branch? item)
+    {
+        Guard.Against.Null(item, nameof(item));
+
+        return new BranchResponse
+        {
+            VanityId = item.VanityId,
+            Name = item.Name,
+            Address = item.Address,
+            SelectedDepartments = item.Departments.Select(b => b.VanityId).ToArray()
+        };
+    }
+
+    /// <summary>
+    /// Maps IEnumerable&lt;Branch&gt; to IEnumerable&lt;BranchResponse&gt; object
+    /// </summary>
+    /// <param name="items"></param>
+    /// <returns></returns>
+    public static IEnumerable<BranchResponse> MapToResponse(this IEnumerable<Branch>? items)
+    {
+        Guard.Against.Null(items, nameof(items));
+
+        return items.Select(branch => new BranchResponse
+        {
+            VanityId = branch.VanityId,
+            Name = branch.Name,
+            Address = branch.Address,
+            SelectedDepartments = branch.Departments.Select(b => b.VanityId).ToArray()
+        });
+    }
+
+    /// <summary>
+    /// Maps a BranchRequest to a Branch entity
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static Branch MapToEntity(this BranchRequest item, Clinic? clinic)
+    {
+        Guard.Against.Null(clinic, nameof(clinic));
+
+        return new Branch
+        {
+            VanityId = item.VanityId,
+            Name = item.Name,
+            Address = item.Address,
+            ClinicId = clinic.Id,
+        };
+    }
+
+    /// <summary>
+    /// Maps a BranchRequest to an existing Branch entity
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public static Branch MapToEntity(this BranchRequest item, Branch? branch, Clinic? clinic)
+    {
+        Guard.Against.Null(branch, nameof(branch));
+        Guard.Against.Null(clinic, nameof(clinic));
+
+        branch.Name = item.Name;
+        branch.Address = item.Address;
+        branch.ClinicId = clinic.Id;
+
+        return branch;
+    }
+}
---
+++ src/ClinicManagement.ApplicationCore/Models/Responses/BranchResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicManagement.ApplicationCore.Models.Responses;
+
+public class BranchResponse : ResponseBase
+{
+    public string Name { get; set; } = string.Empty;
+
+    public Address Address { get; set; } = new Address();
+
+    public Guid ClinicId { get; set; }
+
+    public IEnumerable<Guid> SelectedDepartments { get; set; } = new List<Guid>();
+}
