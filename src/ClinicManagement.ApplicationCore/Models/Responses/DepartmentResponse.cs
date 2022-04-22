132a10e1a3fd1b15e4f6d003402818b138d1307a
Davidson Sousa <dev@davidsonsousa.net>
2022-04-22 11:22:40 +0200

    Added request/response classes

---
+++ src/ClinicManagement.ApplicationCore/Models/Responses/DepartmentResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ClinicManagement.ApplicationCore.Models.Responses;
+
+public class DepartmentResponse : ResponseBase
+{
+    public string Name { get; set; } = string.Empty;
+
+    public Guid BranchId { get; set; }
+}
