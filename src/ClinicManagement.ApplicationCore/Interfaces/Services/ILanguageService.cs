39bf64eacf34917a02e40d24dc100b31a2931688
Davidson Sousa <dev@davidsonsousa.net>
2022-04-25 16:21:57 +0200

    Added service and mapper

---
+++ src/ClinicManagement.ApplicationCore/Interfaces/Services/ILanguageService.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicManagement.ApplicationCore.Interfaces.Services;
+
+public interface ILanguageService : IService<Language>
+{
+    Task<IResult> GetAllLanguages(CancellationToken cancellationToken = default);
+
+    Task<IResult> GetLanguageById(Guid id, CancellationToken cancellationToken = default);
+
+    Task<IResult> SaveAsync(LanguageRequest model, CancellationToken cancellationToken = default);
+}
