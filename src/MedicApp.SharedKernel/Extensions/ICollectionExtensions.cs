201a43e0aeafc6c5fc81d7463b42f4dea9bc36f6
Davidson Sousa <dev@davidsonsousa.net>
2022-04-30 02:09:40 +0200

    Added AddRange extension method to ICollection

---
+++ src/MedicApp.SharedKernel/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace MedicApp.SharedKernel.Extensions;
+
+public static class ICollectionExtensions
+{
+    public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T>? source)
+    {
+        Guard.Against.Null(source, nameof(source));
+
+        if (destination is List<T> list)
+        {
+            list.AddRange(source);
+        }
+        else
+        {
+            foreach (var item in source)
+            {
+                destination.Add(item);
+            }
+        }
+    }
+}
