2f84db7e033418c45b03795803d3a574d5acd101
Davidson Sousa <dev@davidsonsousa.net>
2022-05-01 14:58:54 +0200

    Added EmployeeRepository

---
+++ src/ClinicManagement.ApplicationCore/Interfaces/Data/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ClinicManagement.ApplicationCore.Interfaces.Data;
+
+public interface IEmployeeRepository<TEntity> : IPersonRepository<TEntity>, IRepository<TEntity> where TEntity : Employee
+{
+    Task AddDepartmentsToEmployeeAsync(TEntity? employee, IEnumerable<Department>? departments, CancellationToken cancellationToken = default);
+
+    Task<TEntity?> GetEmployeeWithDepartmentsAndLanguagesById(Guid id, CancellationToken cancellationToken = default);
+
+    Task RemoveDepartmentsFromEmployeeAsync(TEntity? employee, IEnumerable<Department>? departments, CancellationToken cancellationToken = default);
+
+    Task UpdateEmployeeDepartmentsAsync(TEntity? employee, IEnumerable<Department>? departments, CancellationToken cancellationToken = default);
+
+}
