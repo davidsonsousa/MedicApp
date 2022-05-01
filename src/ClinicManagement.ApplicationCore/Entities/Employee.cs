cb0371955b868bd433a31b93ed6f79ef3297cbc9
Davidson Sousa <dev@davidsonsousa.net>
2022-05-01 13:00:20 +0200

    Added Employee abstract type

---
+++ src/ClinicManagement.ApplicationCore/Entities/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace ClinicManagement.ApplicationCore.Entities;
+
+public abstract class Employee : Person
+{
+    public virtual ICollection<Department> Departments { get; set; } = null!;
+
+    public virtual ICollection<WorkSchedule> WorkSchedules { get; set; } = null!;
+}
