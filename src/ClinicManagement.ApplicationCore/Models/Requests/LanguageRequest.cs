92bab122623020b8cad3f2827619402a590ee57d
Davidson Sousa <dev@davidsonsousa.net>
2022-05-07 13:01:19 +0200

    Set request properties as required

--- src/ClinicManagement.ApplicationCore/Models/Requests/LanguageRequest.cs
+++ src/ClinicManagement.ApplicationCore/Models/Requests/LanguageRequest.cs
@@ -2,7 +2,9 @@
 
 public class LanguageRequest : RequestBase
 {
+    [Required]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
     public string Code { get; set; } = string.Empty;
 }
--- src/MedicApp.SharedKernel/PhoneNumber.cs
+++ src/MedicApp.SharedKernel/PhoneNumber.cs
@@ -2,7 +2,9 @@
 
 public class PhoneNumber : ValueObject<PhoneNumber>
 {
+    [Required]
     public string CountryCode { get; set; } = string.Empty;
 
+    [Required]
     public string Number { get; set; } = string.Empty;
 }
