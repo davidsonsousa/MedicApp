3117bd313e6a973188b32cff53ac4e6006f3ea1f
Davidson Sousa <dev@davidsonsousa.net>
2022-06-20 11:16:22 +0200

    Added WebApp project with Clinic data management

---
+++ src/ClinicManagement.WebApp/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using ClinicManagement.WebApp;
+global using MedicApp.SharedServices.Api;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
