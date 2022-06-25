a3e90732609948ccac76170be7dd3dd8e70795bf
Davidson Sousa <dev@davidsonsousa.net>
2022-06-25 22:49:55 +0200

    Added branches and departmentsto API Service

--- src/MedicApp.SharedServices/Api/ClinicManagementApiService.cs
+++ src/MedicApp.SharedServices/Api/ClinicManagementApiService.cs
@@ -12,18 +12,93 @@ public class ClinicManagementApiService : IClinicManagementApiService
         logger = loggerFactory.CreateLogger(nameof(ClinicManagementApiService));
     }
 
-    public async Task<bool> DeleteItemAsync(Guid id)
+    public async Task<bool> DeleteClinicAsync(Guid id)
     {
-        var response = await httpClient.DeleteAsync($"{baseUrl}/clinic/{id}");
+        return await DeleteItemAsync("clinic", id);
+    }
+
+    public async Task<T> GetClinicByIdAsync<T>(Guid id)
+    {
+        return await GetItemByIdAsync<T>("clinic", id);
+    }
+
+    public async Task<IEnumerable<T>> GetClinicsAsync<T>()
+    {
+        return await GetItemsAsync<T>("clinic");
+    }
+
+    public async Task<bool> InsertClinicAsync<T>(T model)
+    {
+        return await InsertItemAsync("clinic", model);
+    }
+
+    public async Task<bool> UpdateClinicAsync<T>(T model)
+    {
+        return await UpdateItemAsync("clinic", model);
+    }
+
+    public async Task<bool> DeleteBranchAsync(Guid id)
+    {
+        return await DeleteItemAsync("branch", id);
+    }
+
+    public async Task<T> GetBranchByIdAsync<T>(Guid id)
+    {
+        return await GetItemByIdAsync<T>("branch", id);
+    }
+
+    public async Task<IEnumerable<T>> GetBranchesAsync<T>()
+    {
+        return await GetItemsAsync<T>("branch");
+    }
+
+    public async Task<bool> InsertBranchAsync<T>(T model)
+    {
+        return await InsertItemAsync("branch", model);
+    }
+
+    public async Task<bool> UpdateBranchAsync<T>(T model)
+    {
+        return await UpdateItemAsync("branch", model);
+    }
+
+    public async Task<bool> DeleteDepartmentAsync(Guid id)
+    {
+        return await DeleteItemAsync("department", id);
+    }
+
+    public async Task<T> GetDepartmentByIdAsync<T>(Guid id)
+    {
+        return await GetItemByIdAsync<T>("department", id);
+    }
+
+    public async Task<IEnumerable<T>> GetDepartmentsAsync<T>()
+    {
+        return await GetItemsAsync<T>("department");
+    }
+
+    public async Task<bool> InsertDepartmentAsync<T>(T model)
+    {
+        return await InsertItemAsync("department", model);
+    }
+
+    public async Task<bool> UpdateDepartmentAsync<T>(T model)
+    {
+        return await UpdateItemAsync("department", model);
+    }
+
+    private async Task<bool> DeleteItemAsync(string endpoint, Guid id)
+    {
+        var response = await httpClient.DeleteAsync($"{baseUrl}/{endpoint}/{id}");
 
         return response.IsSuccessStatusCode
                ? true
                : throw new HttpRequestException($"Delete operation failed. Status code: {response.StatusCode}");
     }
 
-    public async Task<T> GetItemByIdAsync<T>(Guid id)
+    private async Task<T> GetItemByIdAsync<T>(string endpoint, Guid id)
     {
-        var response = await httpClient.GetFromJsonAsync<T>($"{baseUrl}/clinic/{id}");
+        var response = await httpClient.GetFromJsonAsync<T>($"{baseUrl}/{endpoint}/{id}");
 
         if (response is null)
         {
@@ -33,9 +108,9 @@ public class ClinicManagementApiService : IClinicManagementApiService
         return response;
     }
 
-    public async Task<IEnumerable<T>> GetItemsAsync<T>()
+    private async Task<IEnumerable<T>> GetItemsAsync<T>(string endpoint)
     {
-        var response = await httpClient.GetFromJsonAsync<IEnumerable<T>>($"{baseUrl}/clinic");
+        var response = await httpClient.GetFromJsonAsync<IEnumerable<T>>($"{baseUrl}/{endpoint}");
 
         if (response is null)
         {
@@ -45,22 +120,22 @@ public class ClinicManagementApiService : IClinicManagementApiService
         return response;
     }
 
-    public async Task<bool> InsertItemAsync<T>(T model)
+    private async Task<bool> InsertItemAsync<T>(string endpoint, T model)
     {
-        var response = await httpClient.PostAsJsonAsync($"{baseUrl}/clinic", model);
+        var response = await httpClient.PostAsJsonAsync($"{baseUrl}/{endpoint}", model);
 
         return response.IsSuccessStatusCode
                ? true
-               : throw new HttpRequestException($"Insert operation failed. Status code: {response.StatusCode}");
+               : throw new HttpRequestException($"Post operation failed. Status code: {response.StatusCode}");
     }
 
-    public async Task<bool> UpdateItemAsync<T>(T model)
+    private async Task<bool> UpdateItemAsync<T>(string endpoint, T model)
     {
-        var response = await httpClient.PutAsJsonAsync($"{baseUrl}/clinic", model);
+        var response = await httpClient.PutAsJsonAsync($"{baseUrl}/{endpoint}", model);
 
         return response.IsSuccessStatusCode
                ? true
-               : throw new HttpRequestException($"Insert operation failed. Status code: {response.StatusCode}");
+               : throw new HttpRequestException($"Put operation failed. Status code: {response.StatusCode}");
     }
 
     //private async Task<string> PrepareClient()
--- src/MedicApp.SharedServices/Api/IClinicManagementApiService.cs
+++ src/MedicApp.SharedServices/Api/IClinicManagementApiService.cs
@@ -2,13 +2,33 @@
 
 public interface IClinicManagementApiService
 {
-    Task<bool> DeleteItemAsync(Guid id);
+    Task<bool> DeleteClinicAsync(Guid id);
 
-    Task<T> GetItemByIdAsync<T>(Guid id);
+    Task<T> GetClinicByIdAsync<T>(Guid id);
 
-    Task<IEnumerable<T>> GetItemsAsync<T>();
+    Task<IEnumerable<T>> GetClinicsAsync<T>();
 
-    Task<bool> InsertItemAsync<T>(T model);
+    Task<bool> InsertClinicAsync<T>(T model);
 
-    Task<bool> UpdateItemAsync<T>(T model);
+    Task<bool> UpdateClinicAsync<T>(T model);
+
+    Task<bool> DeleteBranchAsync(Guid id);
+
+    Task<T> GetBranchByIdAsync<T>(Guid id);
+
+    Task<IEnumerable<T>> GetBranchesAsync<T>();
+
+    Task<bool> InsertBranchAsync<T>(T model);
+
+    Task<bool> UpdateBranchAsync<T>(T model);
+
+    Task<bool> DeleteDepartmentAsync(Guid id);
+
+    Task<T> GetDepartmentByIdAsync<T>(Guid id);
+
+    Task<IEnumerable<T>> GetDepartmentsAsync<T>();
+
+    Task<bool> InsertDepartmentAsync<T>(T model);
+
+    Task<bool> UpdateDepartmentAsync<T>(T model);
 }
