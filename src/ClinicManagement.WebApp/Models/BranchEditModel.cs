1b78d8cac946b42f8a439a97d0e8345855eb3927
Davidson Sousa <dev@davidsonsousa.net>
2022-06-27 23:53:14 +0200

    Added clinic id and address to branch

--- src/ClinicManagement.WebApp/Models/BranchEditModel.cs
+++ src/ClinicManagement.WebApp/Models/BranchEditModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ClinicManagement.WebApp.Models;
+﻿namespace ClinicManagement.WebApp.Models;
 
 public class BranchEditModel
 {
@@ -8,5 +6,10 @@ public class BranchEditModel
     public Guid VanityId { get; set; }
 
     [Required]
+    public Guid ClinicId { get; set; }
+
+    [Required]
     public string Name { get; set; } = string.Empty;
+
+    public Address Address { get; set; } = new Address();
 }
