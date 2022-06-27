24b341c926a987c1250a249b05cecb68996be266
Davidson Sousa <dev@davidsonsousa.net>
2022-06-27 23:52:14 +0200

    Added usings

--- src/ClinicManagement.WebApp/GlobalUsings.cs
+++ src/ClinicManagement.WebApp/GlobalUsings.cs
@@ -1,4 +1,6 @@
-﻿global using ClinicManagement.WebApp;
+﻿global using System.ComponentModel.DataAnnotations;
+global using ClinicManagement.WebApp;
+global using MedicApp.SharedKernel;
 global using MedicApp.SharedServices.Api;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
